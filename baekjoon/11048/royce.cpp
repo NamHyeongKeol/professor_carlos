fd90cd465dcbb8db99572dbe393a020f95a32cf7
royce <royce@hpcnt.com>
2019-03-21 21:18:25 +0900

    royce 11048

--- baekjoon/11048/royce.cpp
+++ baekjoon/11048/royce.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 using namespace std;
 int a[1002][1002];
+int b[1002][1002];
 int main(){
     int n, m, i, j;
     scanf("%d%d",&n,&m);
@@ -11,4 +12,12 @@ int main(){
             scanf("%d",&a[i][j]);
         }
     }
+
+    for(i=1;i<=n;i++){
+        for(j=1;j<=m;j++){
+        	int zz = max(max(b[i-1][j], b[i][j-1]), b[i-1][j-1]);
+            b[i][j] = zz + a[i][j];
+        }
+    }
+	printf("%d", b[n][m]);
 }
